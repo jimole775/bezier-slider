f9b160b2f81c5ab627926caa113ca363ffdda84d
Rongxis <727346595@qq.com>
2026-06-12 19:31:42 +0800

    Create react-component.js

---
+++ demo/src/react-component.js
@@ -0,0 +1,5 @@
+import BezierSlider from './components/BezierSlider.jsx';
+
+export { BezierSlider };
+export { renderDefaultTrack, ensureDefaultTrackDom, updateDefaultTrack } from './track-renderer.js';
+export default BezierSlider;
