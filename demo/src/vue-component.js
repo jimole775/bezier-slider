44181a202bef85136ec1d1f4c566a0d7f1a16842
Rongxis <727346595@qq.com>
2026-06-12 19:32:32 +0800

    Create vue-component.js

---
+++ demo/src/vue-component.js
@@ -0,0 +1,5 @@
+import BezierSlider from './components/BezierSlider.vue';
+
+export { BezierSlider };
+export { renderDefaultTrack, ensureDefaultTrackDom, updateDefaultTrack } from './track-renderer.js';
+export default BezierSlider;
