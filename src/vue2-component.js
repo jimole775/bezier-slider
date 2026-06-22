06ceae1ce50ba7922aa0b6597c2ee85ab0ab0146
Rongxis <727346595@qq.com>
2026-06-22 15:46:22 +0800

    Create vue2-component.js

---
+++ src/vue2-component.js
@@ -0,0 +1,5 @@
+import BezierSlider from './components/BezierSliderVue2.js';
+
+export { BezierSlider };
+export { renderDefaultTrack, ensureDefaultTrackDom, updateDefaultTrack } from './track-renderer.js';
+export default BezierSlider;
